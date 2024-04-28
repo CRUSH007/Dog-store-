3f5614ef978c13a53fab3fb10bee66efd23c68c1
Crush007 <sanya.tumanow2011@yandex.ru>
2024-04-29 00:13:04 +0400

    все. без  адаптива

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,27 @@
 // Галерея и лайтбоксы от Fancybox
-import { Fancybox } from '@fancyapps/ui';
-import '@fancyapps/ui/dist/fancybox/fancybox.css';
+// import { Fancybox } from '@fancyapps/ui';
+// import '@fancyapps/ui/dist/fancybox/fancybox.css';
 
-Fancybox.bind('[data-fancybox]', {
-	// Your custom options
-});
+// Fancybox.bind('[data-fancybox]', {
+// Your custom options
+// });
 
 // Мобильная навигация
 import mobileNav from './modules/mobile-nav.js';
 mobileNav();
+
+import Swiper from 'swiper/bundle';
+
+// import styles bundle
+import 'swiper/css/bundle';
+
+// init Swiper:
+const swiper = new Swiper('.reviews', {
+	direction: "vertical",
+	mousewheel: true,
+	navigation: {
+		nextEl: ".button-next",
+		prevEl: ".button-prev",
+		disabledClass: "button-disabled",
+	},
+});
